4c4554ff79dc96712108a2d2f1d3dba291cf1cbf
Sourav Mangla <mangla.sourav96@gmail.com>
2019-06-24 16:49:48 +0530

    .

--- socket-server.js
+++ socket-server.js
@@ -3,10 +3,22 @@ var socketIO = require("socket.io");
 
 module.exports = function(server) {
   var io = socketIO(server);
+
   io.on("connection", function(socket) {
     socket.on("chatMessage", function(data) {
+      // console.log( socket.room);
+      io.to(socket.room).emit("chatMessage", data);
+    });
+
+    socket.on("JoinRoom", function(data) {
       console.log(data);
-      io.emit("chatMessage", data);
+      socket.room = data.room;
+      socket.join(data.room);
+      // console.log("from joinRoom", socket.room, data.room, data);
+    });
+
+    socket.on("disconnect", function() {
+      socket.leave(socket.room);
     });
   });
 };
